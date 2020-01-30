1b145b517c0a0455e8f5a8a6bf20ff3eed012bd3
mihaiemanuels <44467370+mihaiemanuels@users.noreply.github.com>
2020-01-30 14:35:26 +0200

    Add files via upload

---
+++ Amazon proj.py
@@ -0,0 +1,19 @@
+# Need a python or Java or C# program that scraps Amazon and outputs results to Google Doc.
+# It need to do the following:
+# Select 5 Products
+# Based on key phrases provided
+# Number of Reviews
+# Average Rating
+# (Essentially getting the best products for the keyphrase)
+# Output: Name, Amazon URL,
+# How it would work:
+# I enter a key phrase
+# Program scraps Amazon using that phrase.
+# It finds the products with the most reviews
+# It then picks the 5 that have the best average rating
+# Produces results with product name & Amazon URL
+# Additional information: Output to Google Doc.
+# Process: Duplicate existing template, Replace Product 1, 2, 3, 4, 5 with Scrapped data.
+# Exceptions:
+# If not enough products found: Not enough products
+# If not enough reviews found: Not enough reviews
